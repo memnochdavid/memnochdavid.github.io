8333eacfb685a7a537d40dac384488c295331823
memnochdavid <davduquediaz@gmail.com>
2026-01-21 15:24:24 +0100

    deploy en github

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Tfg from './pages/Tfg';
 
 export default function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/tfg" element={<Tfg />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
