a839f8adff53d9a745d69f43f27781c04640ddc6
memnochdavid <davduquediaz@gmail.com>
2026-01-22 08:39:25 +0100

    responsive + iframe del pdf dexter + iframe de vídeo de dexter

--- src/includes/Footer.jsx
+++ src/includes/Footer.jsx
@@ -3,14 +3,16 @@ import Powered from '../components/Powered.jsx';
 
 function Footer({ text1, text2, text3, color1 = "white", color2 = "indigo", color3 = "gray" }) {
     return (
-        <footer className="flex flex-col justify-between items-center p-4 border-t
-        border-slate-950 bg-sky-950 min-h-[5vh] gap-2 shadow-[0_-5px_10px_rgba(0,0,0,0.5)]">
-
-
-            <p className={coloresText[color1]?.secundario+" font-extrabold text-2xl tracking-wide"}>{text1}</p>
-            <p className={coloresText[color2]?.secundario+" font-bold text-2xl tracking-wide"}>{text2}</p>
-            <Powered />
-            <p className={coloresText[color3]?.secundario+" font-bold text-sm mt-3"}>{text3}</p>
+        <footer className="flex flex-col md:flex-row justify-between items-center p-4 border-t
+        border-slate-950 bg-sky-950 min-h-[5vh] gap-4 shadow-[0_-5px_10px_rgba(0,0,0,0.5)]">
+            <div className="flex flex-col items-center md:items-start">
+                <p className={coloresText[color1]?.secundario+" font-extrabold text-2xl tracking-wide"}>{text1}</p>
+                <p className={coloresText[color2]?.secundario+" font-bold text-2xl tracking-wide"}>{text2}</p>
+            </div>
+            <div className="flex flex-col items-center md:items-end">
+                <Powered />
+                <p className={coloresText[color3]?.secundario+" font-bold text-sm mt-3"}>{text3}</p>
+            </div>
         </footer>
     );
 }
